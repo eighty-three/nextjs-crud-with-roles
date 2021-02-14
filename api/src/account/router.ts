e81cab1328915e49a3d11433a9f09b6ceec88d96
Eighty Three <contact@eighty-three.dev>
2021-02-14 23:35:50 +0800

    refactor: remove authentication functions

--- api/src/account/router.ts
+++ api/src/account/router.ts
@@ -2,25 +2,11 @@ import express from 'express';
 import validator from '@utils/validator';
 const router = express.Router();
 
-import { authToken, authAccount } from '@authMiddleware/index';
+import { authToken } from '@authMiddleware/index';
 
 import * as account from './controller';
 import * as accountSchema from './schema';
 
-
-router.post('/login',
-  validator(accountSchema.login, 'body'),
-  authToken.verifyExistingToken,
-  authAccount.checkPassword,
-  account.login
-);
-
-router.post('/signup',
-  validator(accountSchema.signup, 'body'),
-  authToken.verifyExistingToken,
-  authAccount.checkExistingUsername,
-  account.signup
-);
 
 router.post('/generate',
   validator(accountSchema.generate, 'body'),
@@ -38,11 +24,6 @@ router.post('/delete',
   validator(accountSchema.deleteAccount, 'body'),
   authToken.verifyRole(['admin']),
   account.deleteAccount
-);
-
-router.post('/logout',
-  validator(accountSchema.logout, 'body'),
-  account.logout
 );
 
 export default router;
