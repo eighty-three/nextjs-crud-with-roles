e33c0e4fe39abcdc36b62a5e3a17b6ac79f74aa6
Eighty Three <contact@eighty-three.dev>
2021-02-14 23:49:01 +0800

    feat: add changePassword endpoint to controller

--- api/src/account/schema.ts
+++ api/src/account/schema.ts
@@ -7,11 +7,17 @@ export const generate = Joi.object({
   role: Joi.string().valid(...roles).required()
 });
 
+export const deleteAccount = Joi.object({
+  username: Joi.string().regex(/^[a-zA-Z0-9_]{1,29}$/).required()
+});
+
 export const changeRole = Joi.object({
   username: Joi.string().regex(/^[a-zA-Z0-9_]{1,29}$/).required(),
   role: Joi.string().valid(...roles).required()
 });
 
-export const deleteAccount = Joi.object({
-  username: Joi.string().regex(/^[a-zA-Z0-9_]{1,29}$/).required()
+export const changePassword = Joi.object({
+  username: Joi.string().regex(/^[a-zA-Z0-9_]{1,29}$/).required(),
+  password: Joi.string().min(1).max(200).required(),
+  newPassword: Joi.string().min(1).max(200).required()
 });
