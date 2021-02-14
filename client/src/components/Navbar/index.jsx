99b82a332320e9afb2ce070d64f13a46e0adc568
Eighty Three <contact@eighty-three.dev>
2021-02-15 07:27:33 +0800

    fix: fix filename of imported CSS file

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from 'next/link';
 
-import styles from './Navbar.module.css';
+import styles from './index.module.scss';
 
 const siteTitle = 'CRUD WITH ROLES';
 
