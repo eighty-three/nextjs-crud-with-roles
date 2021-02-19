edc8cfee72645b353fc5b374601924a938f73901
Eighty Three <contact@eighty-three.dev>
2021-02-19 09:56:10 +0800

    feat: add accounts to props

--- client/src/pages/admin.jsx
+++ client/src/pages/admin.jsx
@@ -7,23 +7,30 @@ import withAuthComponent from '@/components/AuthComponents/withAuth';
 import withAuthServerSideProps from '@/components/AuthComponents/withAuthGSSP';
 import AdminPanelTabs from '@/components/AdminPanelTabs';
 
+import { getAccounts } from '@/lib/account';
+
 const propTypes = {
   username: PropTypes.string,
-  role: PropTypes.string
+  accounts: PropTypes.arrayOf(
+    PropTypes.shape({
+      username: PropTypes.string,
+      role: PropTypes.string
+    })
+  )
 };
 
 const AdminPanel = (props) => {
   const {
-    username
+    username,
+    accounts
   } = props;
-
   return (
     <Layout username={username}>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section>
-        <AdminPanelTabs />
+        <AdminPanelTabs accounts={accounts} />
       </section>
     </Layout>
   );
@@ -32,10 +39,13 @@ const AdminPanel = (props) => {
 AdminPanel.propTypes = propTypes;
 
 export default withAuthComponent(AdminPanel, 'protectRoute', ['admin']);
-export const getServerSideProps = withAuthServerSideProps(async ({ username }) => {
+export const getServerSideProps = withAuthServerSideProps(async ({ username, ctx }) => {
+  const accounts = await getAccounts(ctx);
+
   return {
     props:
       {
+        accounts,
         username
       }
   };
