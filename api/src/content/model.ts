2afe056e03ff15a5fc102564a559d73c34d556ea
Eighty Three <contact@eighty-three.dev>
2021-02-24 21:20:03 +0800

    fix: rework some queries
    Separate some reused queries for optimization
    Allow whole 'post' row to be edited via editPost
    Merge editTags to editPost
    deletePost and editPost should only work if the post is by the user or
    if the user is a 'mod' or an 'admin'

--- api/src/content/model.ts
+++ api/src/content/model.ts
@@ -1,6 +1,6 @@
 import db from '@utils/db';
-import { PreparedStatement as PS } from 'pg-promise';
-import { IPost, IError } from './types';
+import { PreparedStatement as PS, QueryParam } from 'pg-promise';
+import { IPost } from './types';
 
 export const createPost = async (
   username: string,
@@ -8,9 +8,14 @@ export const createPost = async (
   title: string,
   url: string,
   tags?: string[]
-): Promise<IPost|IError> => {
-  try {
-    const tag_ids = (tags) ? await createTags(tags) : [];
+): Promise<IPost> => {
+  return await db.tx(async t => {
+    const tempArr = (tags) ? tags.slice() : [];
+    const tag_ids = (tempArr.length)
+      ? (await t.batch(
+        tempArr.map((tag) => t.one(createTagQuery(tag)))
+      )).map((obj) => Number(obj.tag_id))
+      : [];
 
     const createPost = new PS({ name: 'create-post', text: '\
       WITH ins (user_id, post_id, post, title, url, tags, date) AS ( \
@@ -29,44 +34,15 @@ export const createPost = async (
     });
     createPost.values = [username, post, title, url, tag_ids];
 
-    return await db.task(async t => {
-      const postRow = await t.one(createPost);
-      await t.none(
-        'INSERT INTO posts_tags (post_id, tags) \
-        VALUES ($1, $2)', [postRow.post_id, tag_ids]);
-      delete postRow.post_id;
-      return postRow;
-    });
-  } catch (err) {
-    if (err.message === 'duplicate key value violates unique constraint "posts_url_key"') {
-      return { error: 'The url already exists' };
-    } else {
-      return { error: 'Something went wrong' };
-    }
-  }
-};
+    const postRow = await t.one(createPost);
 
-export const deletePost = async (
-  url: string
-): Promise<void> => {
-  const query = new PS({ name: 'delete-post', text: '\
-    DELETE FROM posts WHERE url=$1'
+    await t.none(
+      'INSERT INTO posts_tags (post_id, tags) \
+      VALUES ($1, $2)', [postRow.post_id, tag_ids]);
+
+    delete postRow.post_id;
+    return postRow;
   });
-
-  query.values = [url];
-  await db.none(query);
-};
-
-export const editPost = async (
-  url: string,
-  post: string
-): Promise<void> => {
-  const query = new PS({ name: 'edit-post', text: '\
-    UPDATE posts SET post=$2 WHERE url=$1'
-  });
-
-  query.values = [url, post];
-  await db.none(query);
 };
 
 export const getPost = async (
@@ -90,28 +66,10 @@ export const getPost = async (
   return await db.oneOrNone(query);
 };
 
-export const getUserPosts = async (
-  username: string,
-  offset: number
-): Promise<IPost[]> => {
-  // Returns posts in the range `offset` to `offset + 10` by account `username`
-  const query = new PS({ name: 'get-user-posts', text: '\
-    SELECT TO_CHAR(p.date, \'Mon DD, YYYY - HH24:MI:SS\') date,\
-      (SELECT array_agg(tag_name) FROM tags WHERE tag_id = ANY(p.tags)) as tags,\
-      p.post, p.title, p.url, a.username, a.name FROM posts p \
-    INNER JOIN accounts a on a.user_id = p.user_id \
-    WHERE a.username=$1\
-    ORDER BY p.date desc LIMIT 21 OFFSET $2'
-  });
-
-  query.values = [username, offset];
-  return await db.manyOrNone(query);
-};
-
 export const getPosts = async (
   offset: number,
-  tag?: string,
-  year?: number
+  tag: string | null,
+  year: number | null
 ): Promise<IPost[]> => {
 
   /* Return latest 13 because the page should only show
@@ -161,7 +119,7 @@ export const getPosts = async (
           p.post, p.title, p.url, a.name FROM posts p \
         INNER JOIN accounts a on a.user_id = p.user_id \
           WHERE DATE_PART(\'year\', p.date) = $1\
-        ORDER BY p.date desc LIMIT 6 OFFSET $2'
+        ORDER BY p.date desc LIMIT 13 OFFSET $2'
       });
 
       query.values = [year, offset];
@@ -173,7 +131,7 @@ export const getPosts = async (
           (SELECT array_agg(tag_name) FROM tags WHERE tag_id = ANY(p.tags)) as tags,\
           p.post, p.title, p.url, a.name FROM posts p \
         INNER JOIN accounts a on a.user_id = p.user_id \
-        ORDER BY p.date desc LIMIT 6 OFFSET $1'
+        ORDER BY p.date desc LIMIT 13 OFFSET $1'
       });
 
       query.values = [offset];
@@ -182,49 +140,136 @@ export const getPosts = async (
   }
 };
 
-export const createTags = async (
-  tags: string[]
-): Promise<number[]> => {
-  if (tags) {
-    const tag_ids = await db.tx(t => {
-      const tagQueries = tags.map((tag) => {
-        /* If the tag is not in the database,
-         * create tag, returning tag_id. Else,
-         * return its tag_id.
-         */
-        const tagQuery = new PS({ name: 'create-tag', text: '\
-          WITH ins AS (\
-            INSERT INTO tags (tag_name)\
-            VALUES ($1)\
-            ON     CONFLICT (tag_name) DO UPDATE\
-            SET    tag_name = NULL \
-            WHERE  FALSE\
-            RETURNING tag_id\
-            )\
-          SELECT tag_id FROM ins\
-            UNION  ALL\
-            SELECT tag_id FROM tags\
-            WHERE  tag_name = $1'
-        });
+export const getUserPosts = async (
+  username: string,
+  offset: number
+): Promise<IPost[]> => {
+  // Returns posts in the range `offset` to `offset + 10` by account `username`
+  const query = new PS({ name: 'get-user-posts', text: '\
+    SELECT TO_CHAR(p.date, \'Mon DD, YYYY - HH24:MI:SS\') date,\
+      (SELECT array_agg(tag_name) FROM tags WHERE tag_id = ANY(p.tags)) as tags,\
+      p.post, p.title, p.url, a.username, a.name FROM posts p \
+    INNER JOIN accounts a on a.user_id = p.user_id \
+    WHERE a.username=$1\
+    ORDER BY p.date desc LIMIT 21 OFFSET $2'
+  });
 
-        tagQuery.values = [tag];
-        return t.one(tagQuery);
-      });
+  query.values = [username, offset];
+  return await db.manyOrNone(query);
+};
 
-      return t.batch(tagQueries);
+export const deletePost = async (
+  username: string,
+  role: string,
+  url: string
+): Promise<void> => {
+  if (role === 'admin' || role === 'mod') {
+    const query = new PS({ name: 'delete-post-force', text: '\
+      DELETE FROM posts WHERE url=$1'
     });
 
-    return tag_ids.map((tag) => Number(tag.tag_id));
-  } else {
-    return [];
+    query.values = [url];
+    await db.none(query);
+  } else if (role === 'user') {
+    const query = new PS({ name: 'delete-post-user', text: '\
+      DELETE FROM posts WHERE url=$1 AND\
+        user_id = (SELECT user_id FROM accounts WHERE username = $2)'
+    });
+
+    query.values = [url, username];
+    await db.none(query);
   }
 };
 
-export const editTags = async (
+export const editPost = async (
+  username: string,
+  role: string,
+  post: string,
+  title: string,
   url: string,
-  tags: string[]
+  newUrl: string,
+  tags?: string[]
 ): Promise<void> => {
-  const tag_ids = await createTags(tags);
+  const query = new PS({ name: 'edit-post', text: '\
+    UPDATE posts\
+      SET post=$1, title=$2, url=$4\
+    WHERE url=$3'
+  });
+
+  query.values = [post, title, url, newUrl];
+  return await db.tx(async t => {
+    const postInfoQuery = getPostInfoQuery(username, url);
+    const postExists = await t.oneOrNone(postInfoQuery);
+
+    const tempArr = (tags) ? tags.slice() : [];
+    const tag_ids = (tempArr.length)
+      ? (await t.batch(
+        tempArr.map((tag) => t.one(createTagQuery(tag)))
+      )).map((obj) => Number(obj.tag_id))
+      : [];
+
+    const queries = (tags) ? editTagsQuery(url, tag_ids) : [];
+    queries.unshift(query);
+
+    if (postExists) {
+      const { editor, creator } = postExists;
+      if (
+        (role === 'admin' || role === 'mod')
+        || ((role === 'user') && (editor === creator))
+      ) {
+        await t.batch(queries.map((query) => t.none(query)));
+      }
+    }
+  });
+};
+
+const getPostInfoQuery = (
+  url: string,
+  username: string
+): QueryParam => {
+  const query = new PS({ name: 'get-post-info', text: '\
+    WITH sel (user_id) AS (\
+      SELECT user_id FROM accounts WHERE username = $2\
+    )\
+    SELECT a.user_id as editor, sel.user_id as creator FROM sel\
+    INNER JOIN posts p on p.url = $1\
+    INNER JOIN accounts a on a.user_id = p.user_id'
+  });
+
+  query.values = [url, username];
+  return query;
+};
+
+const createTagQuery = (
+  tag: string
+): QueryParam => {
+  /* If the tag is not in the database,
+   * create tag, returning tag_id. Else,
+   * return its tag_id.
+   */
+  const query = new PS({ name: 'create-tag', text: '\
+    WITH ins AS (\
+      INSERT INTO tags (tag_name)\
+      VALUES ($1)\
+      ON     CONFLICT (tag_name) DO UPDATE\
+      SET    tag_name = NULL \
+      WHERE  FALSE\
+      RETURNING tag_id\
+      )\
+    SELECT tag_id FROM ins\
+      UNION  ALL\
+      SELECT tag_id FROM tags\
+      WHERE  tag_name = $1'
+  });
+
+  query.values = [tag];
+  return query;
+};
+
+const editTagsQuery = (
+  url: string,
+  tag_ids: number[]
+): QueryParam[] => {
   const updateTagsInPosts = new PS({ name: 'edit-tags-posts', text: '\
     UPDATE posts SET tags = $2\
     WHERE url = $1'
@@ -239,10 +284,5 @@ export const editTags = async (
   });
   updateTagsInPostsTags.values = [url, tag_ids];
 
-  await db.tx(async t => {
-    return await t.batch([
-      t.none(updateTagsInPosts),
-      t.none(updateTagsInPostsTags)
-    ]);
-  });
+  return [updateTagsInPosts, updateTagsInPostsTags];
 };
